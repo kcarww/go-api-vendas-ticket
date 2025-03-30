0e318d5ed2874cab67fb90ee519ba2d87778cc87
kcarww <carlos.oe.cc@gmail.com>
2025-03-29 21:55:17 -0300

    feat: created validated ticked and calculate price for ticket

--- internal/events/domain/ticket.go
+++ internal/events/domain/ticket.go
@@ -1,6 +1,12 @@
 package domain
 
+import "errors"
+
 type TicketType string
+
+var (
+	ErrInvalidTicketKind = errors.New("invalid ticket kind")
+)
 
 const (
 	TicketTypeHalf TicketType = "half"
@@ -13,4 +19,21 @@ type Ticket struct {
 	Spot       *Spot
 	TicketType TicketType
 	Price      float64
+}
+
+func IsValidTicketKind(ticketKind TicketType) bool {
+	return ticketKind == TicketTypeHalf || ticketKind == TicketTypeFull
+}
+
+func (t *Ticket) CalculatePrice() {
+	if t.TicketType == TicketTypeHalf {
+		t.Price /= 2
+	}
+}
+
+func (t *Ticket) Validate() error {
+	if t.Price <= 0 {
+		return errors.New("ticket price must be greater than zero")
+	}
+	return nil
 }
