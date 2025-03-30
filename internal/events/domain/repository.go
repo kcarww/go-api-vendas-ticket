c0407b8145fe93ef3a54a8ba2ca6a36d28df74fc
kcarww <carlos.oe.cc@gmail.com>
2025-03-29 22:30:14 -0300

    feat: created partner factory, repository

--- internal/events/domain/repository.go
+++ internal/events/domain/repository.go
@@ -4,7 +4,7 @@ type EventRepository interface {
 	ListEvents() ([]Event, error)
 	FindEventByID(eventID string) (*Event, error)
 	FindSpotsByEventID(eventID string) ([]*Spot, error)
-	FindSpotByName(eventID, spotName string) (*Spot, error) // Atualizado
+	FindSpotByName(eventID, spotName string) (*Spot, error)
 	// CreateEvent(event *Event) error
 	// CreateSpot(spot *Spot) error
 	// CreateTicket(ticket *Ticket) error
